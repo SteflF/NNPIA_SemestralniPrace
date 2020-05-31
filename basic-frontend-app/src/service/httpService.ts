7c57c3c35628f72cf3f5c0b37eadbb577ed0c71b
František Štefl <stefl.frantisek@gmail.com>
2020-05-31 14:20:31 +0200

    httpService
    Změněno baseURL

--- basic-frontend-app/src/service/httpService.ts
+++ basic-frontend-app/src/service/httpService.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 const config = require('../config.json');
 
-axios.defaults.baseURL = config.server.apiUrl;
+//axios.defaults.baseURL = config.server.apiUrl;
+axios.defaults.baseURL = process.env.REACT_APP_URL;
 
 export default {
     get: axios.get,
